b57396b76310af2486810a0df8d2ab7625ca2999
bm4706 <bm4706@naver.com>
2023-09-04 17:26:15 +0900

    좋아요 팔로우 백엔드에 구현 및 db추가

--- user/models.py
+++ user/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     profile = models.TextField(null=True, blank=True)
+    # 좋아요 기능추가 + Many~를 넣으면 얘가 브릿지테이블을 따로 만들어서 db에 만듬
+    like_todos = models.ManyToManyField("todo.Todo", related_name="like_users")
+    follow = models.ManyToManyField("self", symmetrical=False, related_name="followers")
